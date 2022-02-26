9394f532b7dd011932cee98d95365a4549521da6
bspence205 <brianspencer@spencerbrian.com>
2022-02-25 23:32:04 -0800

    odin dom exercises 1-3

--- script.js
+++ script.js
@@ -1,8 +1,26 @@
-const container = document.querySelector('#container');
+const container = document.querySelector("#container");
+const content = document.createElement("div");
+const paragraph = document.createElement("p");
+const header = document.createElement("h3");
+const borderTest = document.createElement("div");
+const headerOne = document.createElement("h1");
+const para = document.createElement("p");
 
-const content = document.createElement('div');
-content.classList.add('content');
+content.classList.add("content");
 content.textContent = "This is a test, lets hope it works";
-content.style.color = 'blue';
+content.style.color = "blue";
+paragraph.classList.add("test");
+paragraph.style.color = "red";
+paragraph.textContent = "Hi, im red";
+header.style.color = "blue";
+header.textContent = "Hey, im blue blue";
+borderTest.style.cssText = "border: black; background: pink";
+headerOne.textContent = "im a div";
+para.textContent = "ME TOO!";
 
+container.appendChild(paragraph);
 container.appendChild(content);
+container.insertBefore(header, content);
+borderTest.appendChild(headerOne);
+borderTest.appendChild(para);
+container.appendChild(borderTest);
