dbd511313a9daf815942b377a1de2e75cb54aaee
bspence205 <brianspencer@spencerbrian.com>
2022-02-25 21:28:43 -0800

    practiced selecting, creating, styling and appending in DOM

--- script.js
+++ script.js
@@ -1,42 +1,8 @@
-const divv = document.querySelector("button");
+const container = document.querySelector('#container');
 
-const div = document.createElement('div');
+const content = document.createElement('div');
+content.classList.add('content');
+content.textContent = "This is a test, lets hope it works";
+content.style.color = 'blue';
 
-//  Adding inline style methods
-
-div.style.color = "blue";
-
-div.style.cssText = "color: blue; background: black";
-
-div.setAttribute("style", "color: blue; background: red");
-
-// Editing Attributes
-
-div.setAttribute("id", "theDiv");
-// if id exists, update it to 'theDiv', else create an id
-// with value "theDiv"
-
-div.getAttribute("id");
-// returns value of specified attribute, in this case
-// "theDiv"
-
-div.removeAttribute("id");
-// removes specified attribute
-
-// Working with Classes
-
-div.classList.add("new");
-// adds class "new" to your new div
-
-div.classList.remove("new");
-// removes "new" class from div
-
-div.classList.toggle("active");
-// if div doesn't have class "active" then add it, or if
-// it does, then remove it
-
-
-div.textContent = "Hello World!";
-
-div.innerHTML = '<span>Hello World!</span>';
-
+container.appendChild(content);
